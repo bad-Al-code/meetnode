3b1d9b7a46c857b584780f67ec840f60519bfab1
Badal <badalsingh120403@gmail.com>
2025-04-04 13:37:21 -0500

    feat(security): add helmet and basic rate limiting
    - Installs and configures `helmet` middleware to set various
      security-related HTTP headers, improving application resilience
    against
      common web vulnerabilities.
    - Installs and configures `express-rate-limit` middleware to provide
      basic API abuse protection by limiting the number of requests
      per IP address over a time window.

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -7,17 +7,31 @@ import express, {
   NextFunction,
 } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import helmet from 'helmet';
 
 import { NotFoundError } from './utils/errors';
 import { errorHandler } from './middleware/errorHandler.middleware';
 import { setupLogging } from './loaders/logging';
 import { setupSession } from './loaders/session';
+import rateLimit from 'express-rate-limit';
 
 const app: Express = express();
 
-app.set('trust proxy', 2);
+app.set('trust proxy', 1);
+
+app.use(helmet());
 
 setupLogging(app);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: 'Too many request from this IP, please try again after 15 minutes',
+});
+
+app.use(limiter);
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
