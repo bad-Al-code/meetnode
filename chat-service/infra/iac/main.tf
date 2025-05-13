0e05b303998d665d7e74a92f8e6a11b14e6fc6eb
Badal <badalsingh120403@gmail.com>
2025-05-13 06:48:12 -0500

    feat: Add Kubernetes Secret for PostgreSQL password

--- chat-service/infra/iac/main.tf
+++ chat-service/infra/iac/main.tf
@@ -11,3 +11,13 @@ provider "kubernetes" {
   config_path    = "~/.kube/config"
   config_context = "minikube"
 }
+
+resource "kubernetes_secret" "postgres-secret" {
+  metadata {
+    name = "postgres-secret"
+  }
+  type = "Opaque"
+  data = {
+    "postgres-password" : "YzM5ZmFjN2YtM2VlYi00NjlmLTk0NmMtYTNlOTQ3ODA5Njc3"
+  }
+}
